b4fc66a14f4ded57f9975364c78fcbf62e17c98a
s16 <s16@MacBook-Pro-di-s16.local>
2025-10-07 16:38:47 +0200

    Comminit iniziale

---
+++ MetroMilano/MetroMilano/MetroMilanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetroMilanoApp.swift
+//  MetroMilano
+//
+//  Created by s16 on 07/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MetroMilanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
