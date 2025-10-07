16ed1bb893a1602d5fe77eb6cb16906bbcf8c288
s16 <s16@MacBook-Pro-di-s16.local>
2025-10-07 17:43:57 +0200

    Schermata starter

--- MetroMilano/MetroMilano/ContentView.swift
+++ MetroMilano/MetroMilano/ContentView.swift
@@ -5,17 +5,49 @@
 //  Created by s16 on 07/10/25.
 //
 
+//git add .
+//git commit -m "Messaggio del commit"
+//git push origin main
+
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 10) {
+            Spacer()
+            Image(systemName: "tram.fill")
+                .font(.largeTitle)
+                .foregroundStyle(.black)
+            Text("MetroMi")
+                .font(.title2)
+            Spacer()
+            VStack(spacing: 15){
+                Button(action:{}){
+                    Text("Login")
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.black)
+                        .foregroundColor(.white)
+                        .cornerRadius(12)
+                }
+                Button(action: {}){
+                        Text("Sing Up")
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.gray)
+                        .foregroundColor(.black)
+                        .cornerRadius(12)
+                }
+                
+            }
+            .padding(.horizontal)
+            .padding(.bottom, 30)
         }
         .padding()
+        .background(Color(red: 0.96, green: 0.96, blue: 0.96))
     }
 }
 
